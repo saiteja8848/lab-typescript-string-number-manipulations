1bc5a0803cca57df860e079594ed95a8b0759592
saiteja8848 <saiteja8848@gmail.com>
2020-06-08 18:35:02 +0530

    FSJ0420A102

--- started-code/main-service.ts
+++ started-code/main-service.ts
@@ -1,1 +1,11 @@
+export interface StringManipulationService {
+    print(word: string): void;
+    printWithSpace(sentence: string): void;
+    findVowel(str: string): void;
+}
+
+export interface NumberManipulationService {
+    findPrime(num: number): void;
+    findMagic(num: number): void;
+}
 
--- started-code/main.js
+++ started-code/main.js
@@ -1,67 +1,61 @@
-import { StringManipulationService } from "./main-service";
-import { NumberManipulationService } from "./main-service"
-
-class StringManipulations implements StringManipulationService {
-
-    print(word: string): void {
+"use strict";
+exports.__esModule = true;
+var StringManipulations = /** @class */ (function () {
+    function StringManipulations() {
+    }
+    StringManipulations.prototype.print = function (word) {
         console.log("Given string: " + word);
         console.log("Given String in lowerCase: " + word.toLowerCase);
         console.log("Given String in UpperCase: " + word.toUpperCase);
-        for (let i = 0; i < word.length; i++) {
+        for (var i = 0; i < word.length; i++) {
             console.log(word.charAt(i) + " is at this index:" + i);
         }
         console.log("The length of the given String :" + word.length);
-    }
-
-    printWithSpace(sentence: string): void {
-        let n = "";
-        for (let i = 0; i < sentence.length; i++) {
+    };
+    StringManipulations.prototype.printWithSpace = function (sentence) {
+        var n = "";
+        for (var i = 0; i < sentence.length; i++) {
             n += sentence.charAt(i) + " ";
         }
         console.log("Adding Spaces:" + n);
-    }
-
-    findVowel(str: string): void {
-        let count: number = 0;
-        let vowels: Array<string> = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-        for (let i = 0; i < str.length; i++) {
+    };
+    StringManipulations.prototype.findVowel = function (str) {
+        var count = 0;
+        var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        for (var i = 0; i < str.length; i++) {
             if (vowels.indexOf(str.charAt(i)) != -1)
                 count++;
         }
         console.log("No of vowels in " + str + " " + count);
+    };
+    return StringManipulations;
+}());
+var NumbersManipulations = /** @class */ (function () {
+    function NumbersManipulations() {
     }
-
-}
-
-
-class NumbersManipulations implements NumberManipulationService {
-
-
-    findPrime(num: number): void {
-        let flag: number = 0;
-        for (let i = 2; i < num / 2; i++) {
-            if (num % i == 0) { flag = 1; break; }
+    NumbersManipulations.prototype.findPrime = function (num) {
+        var flag = 0;
+        for (var i = 2; i < num / 2; i++) {
+            if (num % i == 0) {
+                flag = 1;
+                break;
+            }
         }
         if (flag == 1)
             console.log(num + " is not a prime number");
         else
             console.log(num + " is  a prime number");
-    }
-
-
-    findSum(n: number): number {
-        let sum: number = 0;
+    };
+    NumbersManipulations.prototype.findSum = function (n) {
+        var sum = 0;
         while (n != 0) {
             sum = sum + Math.floor(n % 10);
             n = Math.floor(n / 10);
         }
         return sum;
-    }
-
-
-
-    findMagic(num: number): void {
-        let sum: number = 0;
+    };
+    NumbersManipulations.prototype.findMagic = function (num) {
+        var sum = 0;
         sum = this.findSum(num);
         //console.log("sum ac" + sum);
         while (true) {
@@ -75,21 +69,15 @@ class NumbersManipulations implements NumberManipulationService {
             console.log('magin no');
         else
             console.log('not a magic no');
-
-
-    }
-
-
-
-}
-
-let stringOperations = new StringManipulations();
+    };
+    return NumbersManipulations;
+}());
+var stringOperations = new StringManipulations();
 console.log("---String Operations!--------");
 stringOperations.findVowel('prograd');
 stringOperations.print('prograd');
 stringOperations.printWithSpace('prograd');
-
-let numberOperations = new NumbersManipulations();
-console.log("------Number Opertaions!--------")
+var numberOperations = new NumbersManipulations();
+console.log("------Number Opertaions!--------");
 numberOperations.findMagic(199);
 numberOperations.findPrime(3);
